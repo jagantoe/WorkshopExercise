1f7c1b2dd821a74402616b37539638cd7f77a134
Ante Coric <ante.coric@involved-it.be>
2024-12-05 12:16:33 +0100

    Add exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, model } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { PEOPLE, Person } from './data';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'WorkshopExercise';
+
+  people: Person[] = PEOPLE;
+  firstChoice = model<Person>();
+  secondChoice = model<Person>();
+  thirdChoice = model<Person>();
+
 }
---
+++ src/app/data.ts
@@ -0,0 +1,15 @@
+export interface Person {
+    id: number;
+    name: string;
+}
+
+export const PEOPLE: Person[] = [
+    { id: 1, name: 'Alice' },
+    { id: 2, name: 'Bob' },
+    { id: 3, name: 'Charlie' },
+    { id: 4, name: 'David' },
+    { id: 5, name: 'Emma' },
+    { id: 6, name: 'Frank' },
+    { id: 7, name: 'Grace' },
+    { id: 8, name: 'Henry' }
+];
